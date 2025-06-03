e2b0820a61140dae6ad2a9ec5498e3c20c130e1a
Terez1st <lacagmal@gmail.com>
2025-06-03 07:07:54 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$server = "localhost";
-$username = "root";
-$password = "";
-$db = "login";
+$server = "127.0.0.1";
+$username = "u352393041_git";
+$password = "Suldan13";
+$db = "u352393041_git";
 
 $conn = new mysqli($server, $username, $password, $db);
 
